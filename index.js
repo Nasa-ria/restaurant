8ddf680e38b1b340c0742fca31ea57d9a5af652d
Nasaria <mumuninasaria@gmail.com>
2021-12-25 18:03:55 +0000

    media query

--- index.js
+++ index.js
@@ -12,4 +12,26 @@ function myFunction() {
       $("header").removeClass("sticky");
     }
   });
-  
+
+  // humberger menu
+  const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav_list");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+// closing menu
+const navLink = document.querySelectorAll(".link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
+ 
